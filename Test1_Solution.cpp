e2a03f0db0f431f79cd7977c72ca1350f1429b25
Marc Tse <32941251+MarcieMarc425@users.noreply.github.com>
2018-02-23 19:10:18 -0500

    Implement plaintext freq. and pos. analysis

--- Test1_Solution.cpp
+++ Test1_Solution.cpp
@@ -1,11 +1,53 @@
+/*
+	Cryptography Project 1 Test 1 Solution
+	Date Due: March 10
+	Authors: Dayan Siddiqui
+	         Nimisha Limaye
+			 Marc Tse
+			 Michael Zhou
+*/
+
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
 
-int main()
-{
+// Method for reading plaintext file and writing char info to vectors of vectors
+// vec[x] contain the characters for each candidate text (space, a-z)
+// vec[x][y] contain the info of chars for each candidate text (occurrences, positions)
+// vec[char_num+27j] to access the characters of candidate text j (from j = 0)
+// vec[char_num+27j][0] = # of occurrences of the character in candidate text j
+// vec[char_num+27j][n] = positions of occurrences where 0 < n < vec[].size()
+//
+void readPlaintext(ifstream& ifs, vector<vector<int>>& vec) {
+	string candidate;
+	int i = 0;
+	int j = 0;
+	while (getline(ifs, candidate)) {
+		if (i > 1 && i % 4 == 0) {
+			for (size_t pos = 0; pos < candidate.length(); pos++) {
+				if (candidate[pos] != 32) {
+					if (vec[(candidate[pos] - 96) + 27 * j].empty())
+						vec[(candidate[pos] - 96) + 27 * j].push_back(0);
+					vec[(candidate[pos] - 96) + 27 * j][0]++;
+					vec[(candidate[pos] - 96) + 27 * j].push_back(pos);
+				}
+				else {
+					if (vec[0 + 27 * j].empty())
+						vec[0 + 27 * j].push_back(0);
+					vec[0][0]++;
+					vec[0].push_back(pos);
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+	return;
+}
+
+int main() {
 
 	ifstream ifs("test1_candidate_5_plaintexts.txt");
 
@@ -14,19 +56,19 @@ int main()
 		exit(1);
 	}
 
-	string line;
-	vector<string> candidate_text;
+	vector<vector<int>> candidate_text(135);
 
-	int i = 0;
-	while (getline(ifs, line)) {
-		if (i > 1 && i % 4 == 0)
-			candidate_text.push_back(line);
-		i++;
+	readPlaintext(ifs, candidate_text);
+
+	// TEST CASE: Find occurrences and positions of 'b' in all candidate texts
+	for (int i = 0; i < 5; i++) {
+		cout << "Candidate Text " << i + 1 << " for 'b':" << endl;
+		cout << "\tOccurrences: " << candidate_text[2 + 27 * i][0] << endl;
+		cout << "\tPositions: ";
+		for (size_t j = 1; j < candidate_text[2 + 27 * i].size(); j++) {
+			cout << candidate_text[2 + 27 * i][j] << " ";
+		}
+		cout << endl;
 	}
-
-	for (size_t i = 0; i < candidate_text.size(); i++) {
-		cout << candidate_text[i] << endl;
-	}
-
 	return 0;
 }
