fd8164939aa5d9973b9791fe1bfe3608ae80b2a0
das800 <das800@users.noreply.github.com>
2018-03-07 19:31:58 -0500

    Update HomophonicEncrypt.cpp

--- HomophonicEncrypt.cpp
+++ HomophonicEncrypt.cpp
@@ -3,13 +3,14 @@
 #include <vector>
 #include <cstdlib>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 int writeIntToFile(const vector<int>& cyphText, ofstream& outFile) {
 	//ofstream outFile("cypherText.txt");
 	if (!outFile) return 0; // if failed
-	for (const auto &e : cyphText) outFile << "\'" << e << "\'" << ", ";
+	for (const auto &e : cyphText) outFile << e << ", ";
 	outFile << "\n";
 	//outFile.close();
 	return 1; //if succeeded
@@ -55,7 +56,7 @@ int enumChars(const char& enumee) {
 }
 
 // takes vector of chars as plaintext, simple cyphertext, and an empty int vect as cyphertext (will append cypher to end if not empty), scheduling algorithm is encoded in here, it is completely random rn but if we need to test other scheduling algorithms i would change it. 
-void encypher(const vector<char>& plainText, const CyphTable& ct, vector<int>& cyphertext) {
+void encypher(const string& plainText, const CyphTable& ct, vector<int>& cyphertext) {
 	vector<vector<int>> keys = ct.keys;
 	for (int i = 0; i < plainText.size(); i++) {
 		int let = enumChars(plainText[i]);
@@ -66,13 +67,13 @@ void encypher(const vector<char>& plainText, const CyphTable& ct, vector<int>& c
 
 void constructRandText(const vector<char>& letterSpace, vector<char>& plainText, int size) {
 	for (int i = 0; i < size; i++) {
-		plainText.push_back(letterSpace[rand() % letterSpace.size() ]);
+		plainText.push_back(letterSpace[rand() % letterSpace.size()]);
 	}
 }
 
 int main() {
 	vector<char> charSpace = { ' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
-	vector<vector<char>> plainTexts;
+	//vector<vector<char>> plainTexts;
 	vector<vector<int>> cyphTexts;
 	CyphTable ct;
 	vector<char> letterSpace;
@@ -82,11 +83,20 @@ int main() {
 		}
 	}
 	
-	for (int i = 0; i < 5; i++) {
+	/*for (int i = 0; i < 5; i++) {
 		vector<char> plainText;
 		constructRandText(letterSpace, plainText, 50);
 		plainTexts.push_back(plainText);
-	}
+	}*/
+	
+	string a = "dipped ligatured cannier cohabitation cuddling coiffeuses pursuance roper eternizes nullo framable paddlings femur bebop demonstrational tuberculoid theocracy women reappraise oblongatae aphasias loftiness consumptive lip neurasthenically dutchmen grift discredited resourcefulness malfeasants swallowed jogger sayable lewder editorials demimondaine tzaritza arrogations wish indisputable reproduces hygrometries gamuts alight borderlines draggle reconsolidated anemometer rowels staggerers grands nu";
+	string b = "rereads predestines equippers cavitation bimolecular lucubrations cabin bettas quiverer prussians cosigner dressier bended dethronement inveigled davenport establish ganges rebroadcast supered bastiles willable abetted motionlessness demonic flatter bunyan securely tippiest tongue aw cotyledonal roomettes underlies miffs inducement overintellectually fertilize spasmodic bacchanal birdbrains decoct snakebite galliard boson headmistress unextended provence weakling pirana fiend lairds argils comma";
+	string c = "trawling responsiveness tastiest pulsed restamps telescoping pneuma lampoonist divas theosophists pustules checkrowed compactor conditionals envy hairball footslogs wasteful conjures deadfall stagnantly procure barked balmier bowery vagary beaten capitalized undersized towpath envisages thermoplastic rationalizers professions workbench underarm trudger icicled incisive oilbirds citrins chambrays ungainliness weazands prehardened dims determinants fishskin cleanable henceforward misarranges fine ";
+	string d = "dean iller playbooks resource anesthetic credibilities nonplus tzetzes incursions stooged envelopments girdling risibility thrum repeaters catheterizing misbestowed cursing malingerers ensconces lippiest accost superannuate slush opinionated rememberer councils mishandling drivels juryless slashers tangent roistering scathing apprenticing fleabite sault achier quantize registrable nobbler sheaf natantly kashmirs dittoes scanned emissivity iodize dually refunded portliest setbacks eureka needines";
+	string e = "mammate punners octette asylum nonclinically trotters slant collocation cardiology enchants ledge deregulated bottommost capsulate biotechnologies subtended cloddiest training joneses catafalque fieldmice hostels affect shrimper differentiations metacarpus amebas sweeter shiatsu oncoming tubeless menu professing apostatizing moreover eumorphic casked euphemistically programmability campaniles chickpea inactivates crossing defoggers reassures tableland doze reassembled striate precocious noncomba";
+
+	vector<string> plainTexts = {a,b,c,d,e};
+
 
 	for (int i = 0; i < plainTexts.size(); i++) {
 		for (int j = 0; j < 5; j++) {
@@ -98,13 +108,14 @@ int main() {
 
 	ofstream outFile("SampleTexts.txt");
 	if (!outFile) cerr << "output file not working";
-	
+
 	outFile << "size: 50\nPlaintexts: 5\nCyphtexts per PT: 5\n\n";
 
 	for (int i = 0; i < plainTexts.size(); i++) {
 		outFile << "Plain Text:\n";
-		writeCharToFile(plainTexts[i], outFile);
-		outFile << "\n Corresponding Cyph Texts:\n" ;
+		//writeCharToFile(plainTexts[i], outFile);
+		outFile << plainTexts[i];
+		outFile << "\n Corresponding Cyph Texts:\n";
 		for (int j = 5 * i; j < ((i + 1) * 5) - 1; j++) {
 			writeIntToFile(cyphTexts[j], outFile);
 		}
